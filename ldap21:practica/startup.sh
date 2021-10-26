4a33b4fe88768462ca823405775d562569b0db09
algamg <91182286+algamg@users.noreply.github.com>
2021-10-26 18:59:31 +0200

    Errors solved

--- ldap21:practica/startup.sh
+++ ldap21:practica/startup.sh
@@ -8,5 +8,6 @@ rm -rf /etc/ldap/slapd.d/*
 rm -rf /var/lib/ldap/*
 slaptest -f /opt/docker/slapd.conf -F /etc/ldap/slapd.d
 slapadd  -F /etc/ldap/slapd.d -l /opt/docker/edt.org.ldif
+slapadd  -F /etc/ldap/slapd.d -l /opt/docker/animal.ldif
 chown -R openldap.openldap /etc/ldap/slapd.d /var/lib/ldap
 /usr/sbin/slapd -d0
